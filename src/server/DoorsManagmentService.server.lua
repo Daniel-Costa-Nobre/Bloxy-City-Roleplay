d50b195f89044e765d4107184865ca60e73b49cb
Daniel Costa Nobre <danielcostanobreprogramador@gmail.com>
2025-07-18 08:17:27 -0300

    Adding new script that will handle door opening and closing

---
+++ src/server/DoorsManagmentService.server.lua
@@ -0,0 +1,12 @@
+-- Important services and modules importing
+local ServerScriptService = game:GetService("ServerScriptService")
+local WorkspaceManager = require(ServerScriptService.WorkspaceManager)
+
+-- Path Instructions
+local PathInstructions = {
+    MinimalHouse = {
+        Door = {
+            "Houses", "MinimalHouse", "Doors", "DOORPLACEHOLDER", ""
+        }
+    }
+}
